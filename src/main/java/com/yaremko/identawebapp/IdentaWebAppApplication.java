d0026140b4e453924c3da4834eca2c1c8dd2a57a
Vitalii Yaremko <yaremkovitalik8@gmail.com>
2023-08-08 16:15:02 +0300

    first commit

---
+++ src/main/java/com/yaremko/identawebapp/IdentaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.yaremko.identawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentaWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaremko/identawebapp/IdentaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaremko.identawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
